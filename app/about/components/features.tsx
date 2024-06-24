0e17202758122ddef85401afa1173ba92153697d
Ryota Kato <rka147@sfu.ca>
2024-06-24 01:51:13 -0700

    add images

--- app/about/components/features.tsx
+++ app/about/components/features.tsx
@@ -12,8 +12,18 @@ export default function Features() {
                     <p>Lorem ipsum dolor sit amet.</p>
                     <button>Download</button>
                 </div>
-                <div className="border rounded-md p-6 shadow">bbb</div>
-                <div className="border rounded-md p-6 shadow">ccc</div>
+                <div className="border relative rounded-md p-6 shadow space-y-3">
+                    <div className="aspect-video bg-muted"><img src="/photograph/pic2.jpg" alt="photo1" className="rounded-lg" /></div>
+                    <h2 className="font-bold">Kayak<Link href={"/"} className="absolute inset-0"></Link></h2>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <button>Download</button>
+                </div>
+                <div className="border relative rounded-md p-6 shadow space-y-3">
+                    <div className="aspect-video bg-muted"><img src="/photograph/pic3.jpg" alt="photo1" className="rounded-lg" /></div>
+                    <h2 className="font-bold">Kayak<Link href={"/"} className="absolute inset-0"></Link></h2>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <button>Download</button>
+                </div>
             </div>  
         </Section>
     );
