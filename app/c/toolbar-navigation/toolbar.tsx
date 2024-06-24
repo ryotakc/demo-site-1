cc4a6977c491e279e6eedb964c0d308eb2a09a55
Ryota Kato <rka147@sfu.ca>
2024-06-24 14:12:48 -0700

    mobile bg-muted

--- app/c/toolbar-navigation/toolbar.tsx
+++ app/c/toolbar-navigation/toolbar.tsx
@@ -8,7 +8,7 @@ import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Toolbar() {
     return (
-        <div className="fixed gap-3 bottom-8 left-1/2 -translate-x-1/2 backdrop-blur h-16 flex items-center border border-border/20 shadow-lg rounded-full p-2 bg-muted/40">
+        <div className="fixed gap-3 bottom-8 left-1/2 -translate-x-1/2 backdrop-blur h-16 flex items-center border border-border/20 shadow-lg rounded-full p-2 bg-muted/40 md:bg-muted/40">
             <UserMenu/>
             <ToolbarButton href="/" icon={<House/>} title="Home"></ToolbarButton>
             <ToolbarButton href="/about" icon={<Image/>} title="Phogoraphy"></ToolbarButton>
